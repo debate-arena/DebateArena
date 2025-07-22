6f80db86c307b48fbfe74d069cea768f1a76d6ee
rlagudtn <rlagudtn@naver.com>
2025-07-22 17:52:44 +0900

    feat: 메인 서비스 프로젝트 생성

---
+++ src/main/java/com/ssafya408/debatearena/DebateArenaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafya408.debatearena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebateArenaApplication.class, args);
+	}
+
+}
