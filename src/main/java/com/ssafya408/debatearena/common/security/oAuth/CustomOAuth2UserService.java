8b2c29f254e1387de468b391f3bbfa8f8010aa02
rlagudtn <rlagudtn@naver.com>
2025-07-23 16:57:04 +0900

    feat: 구글 OAuth2.0, JWT 연동
    - OAuth2.0 연동을 위한 프레임워크를 활용해서 구글의 인증 토큰을 수령
    - 구글 토큰을 받은 후 JWT 토큰을 생성하고 클라리언트에 쿠키로 응답하기 위해 JWT 관련 클래스 구현

---
+++ src/main/java/com/ssafya408/debatearena/common/security/oAuth/CustomOAuth2UserService.java
@@ -0,0 +1,39 @@
+package com.ssafya408.debatearena.common.security.oAuth;
+
+import com.ssafya408.debatearena.db.User;
+import com.ssafya408.debatearena.db.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+    OAuth2User oAuth2User = super.loadUser(userRequest);
+    Custom2OAuthUser customUser = new Custom2OAuthUser(oAuth2User.getAttributes());
+
+    String email = customUser.getEmail();
+    String name = customUser.getName();
+
+    userRepository.findByEmail(email)
+        .orElseGet(()->{
+          User user=User.builder()
+              .email(email)
+              .name(name)
+              .provider(customUser.getProvider())
+              .build();
+
+          return userRepository.save(user);
+        });
+
+    return customUser;
+  }
+}
