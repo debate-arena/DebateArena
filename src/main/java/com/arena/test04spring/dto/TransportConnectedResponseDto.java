d40d86332741cb58709b317fbfa4b013c79ec474
DESKTOP-DLOPF19\SSAFY <wnsdh8846@naver.com>
2025-07-30 13:13:26 +0900

    feat: mediasoup 서버 구축

--- src/main/java/com/arena/test04spring/dto/TransportConnectedResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.arena.test04spring.dto;
-
-import lombok.Data;
-
-@Data
-public class TransportConnectedResponseDto {
-    private String userEmail;
-    private String connectedTransportId;
-}
