ad728de492f0bce03b1eb65b951d9df353491ef1
rlagudtn <rlagudtn@naver.com>
2025-08-14 04:43:48 +0900

    fix: SecurityConfig의 환경 변수 default 값 추가

--- debate/src/main/java/com/ssafya408/debate/domain/common/security/config/SecurityConfig.java
+++ debate/src/main/java/com/ssafya408/debate/domain/common/security/config/SecurityConfig.java
@@ -24,8 +24,7 @@ import java.util.List;
 public class SecurityConfig {
 
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
-  @Value("${cors.allowed-origins}")
+  @Value("${cors.allowed-origins:http://localhost}")
   private List<String> allowedOrigins;
 
   @Bean
