3236a1161db23fa4be74a5fd2ccbce8553233151
rlagudtn <rlagudtn@naver.com>
2025-08-14 21:21:26 +0900

    chore: mathcing 서비스에서 다른 서비스 폴더 제거

--- debate-arena/src/main/java/com/ssafya408/debatearena/service/topic/db/TopicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafya408.debatearena.service.topic.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TopicRepository extends JpaRepository<Topic,Long> {
-
-}
--- debate/src/main/java/com/ssafya408/debate/domain/api/service/DebateStage.java
+++
@@ -1,7 +0,0 @@
-package com.ssafya408.debate.domain.api.service;
-
-public enum DebateStage {
-  OPINION, SIEGE, RESULT;
-
-
-}
